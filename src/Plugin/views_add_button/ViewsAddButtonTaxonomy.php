fe7eb6fd9a2f58564947370bf5f9ec9465f03eb1
terry zhang <zterry@qq.com>
2021-07-18 14:11:04 +0800

    improve taxonomy crud, node crud, user crud, media grid form action

---
+++ src/Plugin/views_add_button/ViewsAddButtonTaxonomy.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\dyniva_backoffice\Plugin\views_add_button;
+
+use Drupal\views_add_button\Plugin\views\field\ViewsAddButtonField;
+use Drupal\Core\Url;
+
+/**
+ * Taxonomy Term plugin for Views Add Button.
+ *
+ * @ViewsAddButton(
+ *   id = "views_add_button_taxonomy_manage",
+ *   label = @Translation("ViewsAddButtonTaxonomy"),
+ *   target_entity = "taxonomy_term"
+ * )
+ */
+class ViewsAddButtonTaxonomy extends ViewsAddButtonField {
+
+  /**
+   * Plugin description.
+   *
+   * @return string
+   *   A string description.
+   */
+  public function description() {
+    return $this->t('Views Add Button URL Generator for Taxonomy Term entities');
+  }
+
+  /**
+   * Check for access to the appropriate "add" route.
+   *
+   * @param string $entity_type
+   *   Entity id as a machine name.
+   * @param string $bundle
+   *   The bundle string.
+   * @param string $context
+   *   Entity context string
+   *
+   * @return bool
+   *   Whether we have access.
+   */
+  public static function checkAccess($entity_type, $bundle, $context) {
+    if ($bundle) {
+      $accessManager = \Drupal::service('access_manager');
+      return $accessManager->checkNamedRoute('entity.taxonomy_term.add_form', ['taxonomy_vocabulary' => $bundle], \Drupal::currentUser());
+    }
+  }
+
+  /**
+   * Generate the add button URL.
+   *
+   * @param string $entity_type
+   *   Entity type ID.
+   * @param string $bundle
+   *   Bundle ID.
+   * @param array $options
+   *   Array of options to be passed to the Url object.
+   * @param string $context
+   *   Module-specific context string.
+   *
+   * @return \Drupal\Core\Url
+   *   Url object which is used to construct the add button link
+   */
+  public static function generateUrl($entity_type, $bundle, array $options, $context = '') {
+    // Create URL from the data above.
+    $url = Url::fromRoute('manage.entity.taxonomy_term.add_form', ['taxonomy_vocabulary' => $bundle], $options);
+    return $url;
+  }
+
+}
--- src/Routing/RouteSubscriber.php
+++ src/Routing/RouteSubscriber.php
@@ -14,25 +14,55 @@ class RouteSubscriber extends RouteSubscriberBase {
    * {@inheritdoc}
    */
   protected function alterRoutes(RouteCollection $collection) {
-    //    dpm($collection->all());
-    //    if ($route = $collection->get('user.page')) {
-    //      $route->setDefault('_controller', '\Drupal\mymodule\Controller\UserController::userPage');
-    //    }
+    //dpm($collection->all());
+    //if ($route = $collection->get('user.page')) {
+    //  $route->setDefault('_controller', '\Drupal\mymodule\Controller\UserController::userPage');
+    //}
+    // User
+    $user_crud = [
+      'user.admin_create' => '/manage/user/add',
+      'entity.user.canonical' => '/manage/user/{user}',
+      'entity.user.edit_form' => '/manage/user/{user}/edit',
+      'entity.user.cancel' => '/manage/user/{user}/cancel',
+    ];
     foreach ($collection->all() as $route_name => $route) {
-      $crud = [
-        'entity.taxonomy_vocabulary.overview_form' => '/manage/taxonomy/{taxonomy_vocabulary}/overview',
-        'entity.taxonomy_term.add_form' => '/manage/taxonomy/{taxonomy_vocabulary}/add',
-        'entity.taxonomy_term.canonical' => '/manage/taxonomy/{taxonomy_term}',
-        'entity.taxonomy_term.edit_form' => '/manage/taxonomy/{taxonomy_term}/edit',
-        'entity.taxonomy_term.delete_form' => '/manage/taxonomy/{taxonomy_term}/delete',
-      ];
-      if (array_key_exists($route_name, $crud)) {
+      if (array_key_exists($route_name, $user_crud)) {
+        $route_clone = clone $route;
+        $route_clone->setPath($user_crud[$route_name]);
+        $collection->add('manage.' . $route_name, $route_clone);
+      }
+    }
+
+    // Node
+    $node_crud = [
+      'node.add' => '/manage/content/{node_type}/add',
+      'entity.node.canonical' => '/manage/content/{node}',
+      'entity.node.edit_form' => '/manage/content/{node}/edit',
+      'entity.node.delete_form' => '/manage/content/{node}/delete',
+    ];
+    foreach ($collection->all() as $route_name => $route) {
+      if (array_key_exists($route_name, $node_crud)) {
+        $route_clone = clone $route;
+        $route_clone->setPath($node_crud[$route_name]);
+        $collection->add('manage.' . $route_name, $route_clone);
+      }
+    }
+
+    // Taxonomy
+    $taxonomy_crud = [
+      'entity.taxonomy_vocabulary.overview_form' => '/manage/taxonomy/{taxonomy_vocabulary}',
+      'entity.taxonomy_term.add_form' => '/manage/taxonomy/{taxonomy_vocabulary}/add',
+      'entity.taxonomy_term.canonical' => '/manage/taxonomy/term/{taxonomy_term}',
+      'entity.taxonomy_term.edit_form' => '/manage/taxonomy/term/{taxonomy_term}/edit',
+      'entity.taxonomy_term.delete_form' => '/manage/taxonomy/term/{taxonomy_term}/delete',
+    ];
+    foreach ($collection->all() as $route_name => $route) {
+      if (array_key_exists($route_name, $taxonomy_crud)) {
         $route_clone = clone $route;
         //route path example: admin/structure/taxonomy/manage/{taxonomy_vocabulary}/add
-        $route_clone->setPath($crud[$route_name]);
+        $route_clone->setPath($taxonomy_crud[$route_name]);
         $collection->add('manage.' . $route_name, $route_clone);
       }
     }
   }
-
 }
